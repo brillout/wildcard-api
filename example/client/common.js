4658eeef8155eee1c08c20c4600f10f0eaefa464
Romuald Brillout <github.com@brillout.com>
2019-09-20 17:27:15 +0200

    do not use argumentsAlwaysInHttpBody in example

--- example/client/common.js
+++ example/client/common.js
@@ -1,8 +1,10 @@
 import "babel-polyfill";
 import {endpoints} from 'wildcard-api/client';
-import wildcardClient from 'wildcard-api/client';
 
+/*
+import wildcardClient from 'wildcard-api/client';
 wildcardClient.argumentsAlwaysInHttpBody = true;
+*/
 
 // Make endpoints available to play with in the dev console
 window.endpoints = endpoints;
