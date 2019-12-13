bebafccceb3518180bc9fa66cc05d74406707c81
Romuald Brillout <github.com@brillout.com>
2019-12-13 10:46:28 +0100

    also autoload `endpoints.*` files

--- server/autoload.js
+++ server/autoload.js
@@ -1,7 +1,8 @@
 const {projectDir, findFiles} = require('@brillout/project-files');
 
-const endpointFiles = findFiles('*.endpoints.*');
-
-endpointFiles.forEach(endpointFile => {
+[
+  ...findFiles('*.endpoints.*'),
+  ...findFiles('endpoints.*'),
+].forEach(endpointFile => {
   require(endpointFile);
 });
