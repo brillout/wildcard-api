6e342cc3b1d0460eb9900278ddd9723e34a2bcd8
Romuald Brillout <github.com@brillout.com>
2020-11-22 17:40:30 +0100

    wip

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// TODO write test for this
+throw new Error(
+  '[Telefunc][Wrong Usage] You cannot `require("telefunc")`/`import * from "telefunc"`. Either `require("telefunc/client")`/`import * from "telefunc/client"` or `require("telefunc/server")`/`import * from "telefunc/server"` instead.'
+);
