f7e20686284c3d5b5b1dd79c56e2f66b05180008
Romuald Brillout <github.com@brillout.com>
2020-12-06 15:57:19 +0100

    make telefunc client always available on the window global object

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,16 @@
 import { telefuncClient } from "./global-instance";
 export const server = telefuncClient.endpoints;
 export const { config } = telefuncClient;
-export { context } from "./sessions";
+import { context } from "./sessions";
+export { context };
+
+window.telefunc = {
+  context,
+  server,
+  config,
+} as never;
+declare global {
+  interface Window {
+    telefunc: never;
+  }
+}
