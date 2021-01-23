b79dbee096f6d6f1d9cfc59c9d26dff4175303dd
Romuald Brillout <git@brillout.com>
2021-01-23 10:50:56 +0100

    [code-removal] remove prisma database seed & migration, and include database binary instead

--- examples/prisma/server.ts
+++ examples/prisma/server.ts
@@ -1,16 +1,13 @@
 import express = require("express");
 import { telefunc } from "telefunc/server/express";
-import { seed } from "./database/seed";
 
 startServer();
 
-async function startServer() {
+function startServer() {
   const app = express();
 
   app.use(telefunc());
   app.use(express.static("dist/", { extensions: ["html"] }));
-
-  await seed();
 
   app.listen(3000, () =>
     console.log("Server ready, go to http://localhost:3000")
