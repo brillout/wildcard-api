1cd0a7d2ff4df4f686cd8a62d0f58baffce5f9b7
Romuald Brillout <git@brillout.com>
2021-01-25 00:34:26 +0100

    minor

---
+++ src/client/serverUntyped.js
@@ -0,0 +1,3 @@
+// See `contextUntyped.js` for an explanation of what this file is about.
+const global_instance = require("./global-instance");
+exports.server = global_instance.telefuncClient.telefunctions;
