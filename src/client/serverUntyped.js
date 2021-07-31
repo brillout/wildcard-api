5736c5fa075fa58a5c3b05b6856b242c1347ae0f
Romuald Brillout <git@brillout.com>
2021-07-31 11:09:37 +0200

    [code-removal] remove untyped export of context and server

--- src/client/serverUntyped.js
+++
@@ -1,3 +0,0 @@
-// See `contextUntyped.js` for an explanation of what this file is about.
-const global_instance = require("./global-instance");
-exports.server = global_instance.telefuncClient.telefunctions;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTelefuncCaller = exports.getContext = void 0;
-var getContext_1 = require("./server/getContext");
-Object.defineProperty(exports, "getContext", { enumerable: true, get: function () { return getContext_1.getContext; } });
-var createTelefuncCaller_1 = require("./server/createTelefuncCaller");
-Object.defineProperty(exports, "createTelefuncCaller", { enumerable: true, get: function () { return createTelefuncCaller_1.createTelefuncCaller; } });
-//# sourceMappingURL=index.js.map
